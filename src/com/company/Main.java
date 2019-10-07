0d712e94b727d65c3b5c90b861da578c8b25fc0d
Robert <robert.przybyla94@gmail.com>
2019-10-07 20:28:00 +0200

    Zmieniony plik main 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello-umieszczony na githubie");
+        System.out.println("Hello-nowa wersja");
 	// write your code here
     }
 }
